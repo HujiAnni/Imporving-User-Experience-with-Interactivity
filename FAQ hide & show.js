5083aa68c8d01529e9134a8c81e93e42953858d8
Jianni Hu <65553010+HujiAnni@users.noreply.github.com>
2020-08-28 03:56:24 +0800

    Add files via upload

---
+++ FAQ hide & show.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+  $("#q1").on('click', function() {
+    if ($("#q1 .less").is(":hidden")){
+      $("#q1 .less").removeClass("hidden");
+      $("#q1 .more").addClass("hidden");
+    } else {
+      $("#q1 .less").hide();
+      $("#q1 .more").show();
+    }
+  });
+
+  $("#q2").on('click', function() {
+    if ($("#q2 .less").is(":hidden")){
+      $("#q2 .less").removeClass("hidden");
+      $("#q2 .more").addClass("hidden");
+    } else {
+      $("#q2 .less").hide();
+      $("#q2 .more").show();
+    }
+  });
+
+  $("#q3").on('click', function() {
+    if ($("#q3 .less").is(":hidden")){
+      $("#q3 .less").removeClass("hidden");
+      $("#q3 .more").addClass("hidden");
+    } else {
+      $("#q3 .less").hide();
+      $("#q3 .more").show();
+    }
+  });
+
+  $("#q4").on('click', function() {
+    if ($("#q4 .less").is(":hidden")){
+      $("#q4 .less").removeClass("hidden");
+      $("#q4 .more").addClass("hidden");
+    } else {
+      $("#q4 .less").hide();
+      $("#q4 .more").show();
+    }
+  });
+
+  $("#q5").on('click', function() {
+    if ($("#q5 .less").is(":hidden")){
+      $("#q5 .less").removeClass("hidden");
+      $("#q5 .more").addClass("hidden");
+    } else {
+      $("#q5 .less").hide();
+      $("#q5 .more").show();
+    }
+  });
+
+  $("#q6").on('click', function() {
+    if ($("#q6 .less").is(":hidden")){
+      $("#q6 .less").removeClass("hidden");
+      $("#q6 .more").addClass("hidden");
+    } else {
+      $("#q6 .less").hide();
+      $("#q6 .more").show();
+    }
+  });
+
+});
